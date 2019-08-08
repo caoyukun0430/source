667add05b496aaa7715c7e41c94b1559a462251a
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-08-08 10:08:52 +0200

    Modified environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,6 @@ export const environment = {
     enableDownload: true,
     enableLogin: true,
     maxNoVariantsForAlignments: 235,
+    firstPage: '/real-ws/plist',
     loginTextHint: 'Username: admin Password: admin will give you full access to the system. Username: user1 Password: user1 will give you access to the running-example log along with the possibility to download the running-example. Username: user2 Password: user2 will give you access to the receipt log but without the possibility to download the log.'
 };
