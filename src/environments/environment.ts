1322a4a4264625c2f78e9c8ab816d9dfe278f8c1
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-08-08 09:59:33 +0200

    Modifying environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  webServicePath: "http://localhost:5000/",
+  webServicePath: 'http://' + window.location.hostname + ':5000/',
   enableUpload: true,
   enableDownload: true,
   enableLogin: false,
