6b73206dfec9f8cfc46ea481033ee64751546662
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-08-09 10:40:52 +0200

    Added MD5 hashing at front-end by configuration

--- src/app/pages/content-pages/login/login-page.component.ts
+++ src/app/pages/content-pages/login/login-page.component.ts
@@ -1,10 +1,10 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Router, ActivatedRoute } from "@angular/router";
-import { environment } from '../../../../environments/environment';
-import {Pm4pyService} from "../../../pm4py-service.service";
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {Router, ActivatedRoute} from '@angular/router';
+import {environment} from '../../../../environments/environment';
+import {Pm4pyService} from '../../../pm4py-service.service';
 import {AuthenticationServiceService} from '../../../authentication-service.service';
-
+import {Md5} from 'ts-md5/dist/md5';
 
 @Component({
     selector: 'app-login-page',
@@ -13,49 +13,53 @@ import {AuthenticationServiceService} from '../../../authentication-service.serv
 })
 
 export class LoginPageComponent {
-    username : string;
-    password : string;
+    username: string;
+    password: string;
 
-    public loginTextHint : string;
+    public loginTextHint: string;
 
 
     @ViewChild('f') loginForm: NgForm;
 
     constructor(private router: Router,
-        private route: ActivatedRoute, private pm4pyServ: Pm4pyService, private authService: AuthenticationServiceService) {
+                private route: ActivatedRoute, private pm4pyServ: Pm4pyService, private authService: AuthenticationServiceService) {
 
         this.authService.checkAuthentication().subscribe(data => {
             //console.log(data);
         });
 
-        this.username = "";
-        this.password = "";
+        this.username = '';
+        this.password = '';
 
         this.loginTextHint = environment.loginTextHint;
     }
 
     // On submit button click    
     onSubmit() {
-        this.username = (<HTMLInputElement>document.getElementById("username0")).value;
-        this.password = (<HTMLInputElement>document.getElementById("password0")).value;
+        this.username = (<HTMLInputElement>document.getElementById('username0')).value;
+        this.password = (<HTMLInputElement>document.getElementById('password0')).value;
+
+        if (environment.enableMD5cipheringPasswordsFrontend) {
+            const md5 = new Md5();
+            this.password = '' + md5.appendStr(this.password).end();
+        }
 
         this.pm4pyServ.loginService(this.username, this.password).subscribe(data => {
-            let resultJson : JSON = data as JSON;
+            let resultJson: JSON = data as JSON;
             console.log(resultJson);
 
-            if (resultJson["status"] == "OK") {
-                localStorage.setItem("sessionId", resultJson["sessionId"]);
-                localStorage.removeItem("filtersPerProcess");
+            if (resultJson['status'] == 'OK') {
+                localStorage.setItem('sessionId', resultJson['sessionId']);
+                localStorage.removeItem('filtersPerProcess');
                 this.router.navigateByUrl('/real-ws/plist');
-            }
-            else {
-                alert("Login failed! Try again");
+            } else {
+                alert('Login failed! Try again');
             }
         });
     }
 
     keyDownFunction(event) {
-        if(event.keyCode == 13) {
+        if (event.keyCode == 13) {
             this.onSubmit();
             // rest of your code
         }
