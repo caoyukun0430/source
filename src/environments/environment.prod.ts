0a409327352064ceb0e184f81ebb60103afcbbc6
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-08-17 14:34:55 +0200

    Disabling transient analysis

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,7 +10,7 @@ export const environment = {
     loginTextHint: 'Username: admin Password: admin will give you full access to the system. Username: user1 Password: user1 will give you access to the running-example log along with the possibility to download the running-example. Username: user2 Password: user2 will give you access to the receipt log but without the possibility to download the log.',
     overallEnableBPMN: false,
     overallEnableAlignments: true,
-    overallEnableTransient: true,
+    overallEnableTransient: false,
     overallEnableStatistics: true,
     overallEnableSNA: true,
     overallEnableDifferentProcessSchemas: true,
