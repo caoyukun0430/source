feadc73f7e0519d1a7123b27cbbd34951b4fdf96
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-08-19 07:34:25 +0200

    Added uniqueCallId

--- src/app/authentication-service.service.ts
+++ src/app/authentication-service.service.ts
@@ -24,6 +24,14 @@ export class AuthenticationServiceService {
         this.resetAuthentication();
     }
 
+    newGuid() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16 | 0,
+                v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+
     doLogout() {
         this.resetAuthentication();
         localStorage.removeItem("process");
@@ -61,6 +69,7 @@ export class AuthenticationServiceService {
 
         parameters = parameters.set("process", process);
         parameters = parameters.set("session", sessionId);
+        parameters = parameters.set("uniqueCallId", this.newGuid());
 
         var completeUrl : string = this.webservicePath + "checkSessionService";
 
